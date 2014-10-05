3dcffea1e57b8d4fe1fc79c7794efbbbc49dfb8c
Gudbrand <gudbrandduff@gmail.com>
2014-10-05 18:48:27 +0200

    bildene begynner å ta form

---
+++ dev/xmlparser.py
@@ -0,0 +1,24 @@
+# Skript som skriver ut filstrukturen i ../model/bilder mappen til en xml-fil
+
+import glob
+
+bilderurl = "../model/bilder"
+xmlfile = open("../model/bilder.xml", "w")
+
+xmlfile.write("<BILDER>\n")
+
+for albumpath in glob.glob(bilderurl+"/*"):
+	path = albumpath.split("/")
+	album = path[-1]
+	
+	xmlfile.write("""  <ALBUM ID='"""+album+"""'>\n""")
+
+	for bildepath in glob.glob(albumpath+"/*"):
+		path = bildepath.split("/")
+		bilde = path[-1]
+		xmlfile.write("    <BILDE>"+bilde+"</BILDE>\n")
+		#xmlfile.write("      </KOMMENTAR>\n")
+
+	xmlfile.write("  </ALBUM>\n")
+
+xmlfile.write("</BILDER>\n")
