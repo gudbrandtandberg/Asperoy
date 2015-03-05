f99272c1b1d7604756eddd5c14a4a80a97c84f88
Gudbrand <gudbrandduff@gmail.com>
2015-03-05 21:24:11 +0100

    startet bootstrapping og segregerer styles

--- public/views/templates/header.php
+++ public/views/templates/header.php
@@ -4,7 +4,7 @@
 //Tar seg først av all head som gjelder for alle sidene. Inneholder det mest generelle av javascript
 //Og visningen av hele headeren, inkl. barn.
 //
-//Åpen <div class = innhold> lukkes i footer.php
+//Åpen <div class = containter-fluid> lukkes i footer.php
 
 if (!isset($_SESSION)) {
     session_start(); 		// nødvendig for å ha tilgang til $_SESSION variablen
@@ -19,17 +19,18 @@ if (!$_SESSION['loggedIn']) {
     <head>
             <meta charset="utf-8">
             <title><?=$title;?></title>
-            <link rel="shortcut icon" href="/resources/images/asperøyico.ico" type="image/x-icon">
-            <link rel="icon" href="/resources/images/asperøyico.ico" type="image/x-icon">
-            <link rel="stylesheet" href="/styles/style.css"</link>
+            <link rel="shortcut icon" href="/resources/images/asperøyico.ico" type="image/x-icon"/>
+            <link rel="icon" href="/resources/images/asperøyico.ico" type="image/x-icon"/>
+	    <link rel="stylesheet" type="text/css" href="/styles/bootstrap/bootstrap.css"/>
+            <link rel="stylesheet" type="text/css" href="/styles/style.css"</link>
+	    <link rel="stylesheet" type="text/css" href="/styles/hjemStyle.css"</link>
+	    <link rel="stylesheet" type="text/css" href="/styles/loginStyle.css"</link>
             <link rel="stylesheet" type="text/css" href="/js/slick/slick.css"/>
-<!--            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
             <script src="/js/jquery-1.11.1.min.js"></script>
-
+	    <script src="/js/boostrap.min.js"></script>
+	    
             <script type="text/javascript">
                 $(document).ready(function(){
-                        
-                        //få tilbake animeringen her...
                         
                         $(".knapp").mouseenter(function(){
                             $(this).animate({
@@ -43,13 +44,6 @@ if (!$_SESSION['loggedIn']) {
                             });
                             
                         });
-                        
-
-	
-                        $('#forrige').click(function(){
-	   
-                        });
-                        
                         $("#logut").click(function(e){
                             e.preventDefault();
                             
@@ -61,31 +55,51 @@ if (!$_SESSION['loggedIn']) {
     </head>
 
     <body>
-        <div id="holder">
+        <div id="wrapper">
             <div id="header">
-                    <div id="tittel">ASPERØY</div>
-                    <img id="logo" src="http://asperoy/resources/images/asperøy_contour.png">
-            
-                    <ul id="meny">
-                            <li class="knapp" id="hjem">
-                                <a href="/hjem/">HJEM</a>
-                            </li>
-                            <li class="knapp" id="bilder">
-                                <a href="/bilder/">BILDER</a>
-                            </li>
-                            <li class="knapp" id="kalender">
-                                <a href="/kalender/">KALENDER</a>
-                            </li>
-                            <li class="knapp" id="prosjekter">
-                                <a href="/prosjekter/">PROSJEKTER</a>
-                            </li>
-                            <li class="knapp" id="ressurser">
-                                <a href="/ressurser/">RESSURSER</a>
-                            </li>
-                            <li class="knapp" id="logut">
-                                <a href="#">LOGG UT</a>
-                            </li>
-                    </ul>
+		<div id="weather">
+		    20^oC lett overskyet
+		</div>
+		
+		<h1 id="tittel">ASPERØY</h1>
+		<img id="logo" src="/resources/images/asperøy_contour.png"/>
+		
+		<nav class="navbar-default navbar">
+		    <div class="container-fluid">
+			<div class="navbar-header">
+			    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			      <span class="sr-only">Toggle navigation</span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			    </button>
+			</div>
+			<div class="navbar-collapse collapse" id="navbar">
+			    <ul class="nav navbar-nav">
+				<li class="knapp" id="hjem">
+				    <a href="/hjem/">HJEM</a>
+				</li>
+				<li class="knapp" id="bilder">
+				    <a href="/bilder/">BILDER</a>
+				</li>
+				<li class="knapp" id="kalender">
+				    <a href="/kalender/">KALENDER</a>
+				</li>
+				<li class="knapp" id="prosjekter">
+				    <a href="/prosjekter/">PROSJEKTER</a>
+				</li>
+				<li class="knapp" id="ressurser">
+				    <a href="/ressurser/">RESSURSER</a>
+				</li>
+			    </ul>
+			    <ul class="nav navbar-nav navbar-right">
+				<li class="knapp" id="logut">
+				    <a href="#">LOGG UT</a>
+				</li>
+			    </ul>
+			</div>
+		    </div>
+		</nav>
             </div>
-            <div id="innhold">
+            <div id="innhold" class="container-fluid">
             
