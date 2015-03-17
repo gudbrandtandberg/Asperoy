cf20d25f816e95b2f238ba45cc691a0f2fcfe634
Gudbrand <gudbrandduff@gmail.com>
2015-03-17 09:06:16 +0100

    stasher litt arbeid

--- public/api/lagreKommentar.php
+++ public/api/lagreKommentar.php
@@ -20,7 +20,7 @@
 ?>
 <div class="kommentar">
     	<div class="kommentarbilde">
-    	    <img src="<?=$fulldir;?>/resources/images/users/avatar.jpg" width="50" alt="Brukerbilde">
+    	    <img src="/resources/images/users/avatar.jpg" width="50" alt="Brukerbilde">
     	</div>
         
     	<div class="kommentarinnhold">
@@ -29,7 +29,7 @@
     	    <div class="kommentarinfo">
     		<span class="dato"><?=$dato;?></span>
     		<a href="like.php">Like</a>
-    		<img src="<?=$fulldir;?>/resources/images/like.jpg" style="display: inline" width="20" alt="Tommel">
+    		<img src="/resources/images/like.jpg" style="display: inline" width="20" alt="Tommel">
     		<span class="numlikes" style="visibility: hidden"></span>
     	    </div>
     	</div>
