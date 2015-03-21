3e35c03f372385b1d0e646f65d406549e97c1634
Gudbrand <gudbrandduff@gmail.com>
2015-03-21 12:40:28 +0100

    småendringer - fikset php og bittelitt styling

--- public/hjem/index.php
+++ public/hjem/index.php
@@ -24,7 +24,7 @@ $eventsInOrder = $kalenderController->getAllFutureEventsSorted();
 <div class="col-xs-12 col-sm-7 side" id="side1">
     <h2>Siste nytt! <small>17.5.2016</small></h2>
     <div class="bildecontainer">
-	<img width="90%" class="img-rounded" height="325px" src="/resources/bilder/Sommer 2013/a.jpg">
+	<img class="img-rounded" height="325px" src="/resources/bilder/Sommer 2013/a.jpg">
     </div>
     <p class="tekst">
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
@@ -38,15 +38,26 @@ $eventsInOrder = $kalenderController->getAllFutureEventsSorted();
         <table id="kalendertable">
             <?php
             $numberOfEvents = count($eventsInOrder);
-            for ($i = 0; $i < ($numberOfEvents < 8 ? $numberOfEvents : 8); $i++) {
-                $event = $eventsInOrder[$i];
-                echo "<tr><td><h4><small>" . date("d.m", strtotime($event->start)) . "</small></h4></td><td><h4>" . $event->title . "</h4></td></tr>";
-            }
-            ?>
+            for ($i = 0; $i < ($numberOfEvents < 8 ? $numberOfEvents : 8); $i++): ?>
+                <?php $event = $eventsInOrder[$i]; ?>
+                <tr>
+		    <td>
+			<h4>
+			    <small><?=date("d.m", strtotime($event->start));?></small>
+			</h4>
+		    </td>
+		    <td>
+			<h4>
+			    <?=$event->title;?> 
+			</h4>
+		    </td>
+		</tr>
+	    <?php endfor; ?>
         </table>
     </div>
     
     <div id="weather" class="col">
+	<h4>Været på Asperøya nå:</h4>
 	<span id="temp" style="height: 23px;"></span>
 	<canvas id="weathericon" width="30" height="30"></canvas>
 	<span id="vind"></span>
