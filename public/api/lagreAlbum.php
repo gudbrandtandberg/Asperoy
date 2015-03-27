84ce88ba8fa92afeb5330f5cf4267e87e50908a0
Gudbrand <gudbrandduff@gmail.com>
2015-03-27 18:50:00 +0100

    endelig, grid som ser bra ut

--- public/api/lagreAlbum.php
+++ public/api/lagreAlbum.php
@@ -1,14 +1,10 @@
-
 <?php
-
-$albumnavn = $_GET["albumnavn"];
-
+    $albumnavn = $_GET["albumnavn"];
 ?>
-<div class='col-xs-6 col-md-3 tommel'>
+
+<div class='col-xs-6 col-md-3'>
     <a class="tommel" href="#">
-        <div class="tommelbildebeholder">
-            <img class="tommelbilde" src="http://www.casa-candy.com/preload-images/default-placeholder.png">
-        </div>
+        <div class="tommelbildebeholder" style="background-image: url('http://www.casa-candy.com/preload-images/default-placeholder.png');"></div>
         <div class="tommelcaption"><?=$albumnavn;?></div>
     </a>
 </div>
