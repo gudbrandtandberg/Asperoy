4185bfdba51ef8a4ff785d5baf432f18b62366f9
Eivind <eivind.m.bakke@gmail.com>
2015-04-10 01:16:47 +0800

    ok, la til noen kommentarer og ryddet litt opp

--- public/js/nybruker.js
+++ public/js/nybruker.js
@@ -44,40 +44,27 @@ function check(i, element, blur) {
 /*http://www.menucool.com/color-picker*/
 /*https://css-tricks.com/html5-drag-and-drop-avatar-changer-with-resizing-and-cropping/*/
 
-function openFile(event){
-    
-    var bildeInput = document.getElementById("bildeinput");
-    var imgElement = document.getElementById("profilbildeimg");
-
-    var file = bildeInput.files[0];
-    
-    //er det egentlig et bilde?
-    if (!file.type.match('image.*')) {
-        alert("Det må være et jpg bilde!");  //lyver litt
-        return;
-    }
-    
-    //laster bildefilen over i et img objekt
-    reader = new FileReader();
-    reader.onload = prepareAndDrawImage;
-    reader.readAsDataURL(file);
-}
-
+// vi maa holde styr paa den orginale bildehoyden og bredden for aa gjore zoom
 var originalImgHeight = 0;
 var originalImgWidth = 0;
+
+// bildekoordinatene i canvasen, etter at brukeren har flyttet paa det. pga. hvordan flytting regnes ut maa vi ha begge de to under
 var imgCoor = {
     x: 0,
     y: 0
 };
+// bildekoordinatene mens det flyttes paa
 var movingImgCoor = {
     x: 0,
     y: 0
 };
+// koordinatene der brukeren trykker ned for aa flytte bilde maa lagres for aa finne ut hvor mye som er flyttet
 var mouseDownCoor = {
     x: 0,
     y: 0
 };
 
+// Tegner et bildeobjekt til canvasen, med en valgfrie x og y coordinater
 function drawImage(img, x, y) {
     var canvas = document.getElementById("redigeringscanvas"); // fordi vi trenger DOM objektet og kan ikke bruke jQuery objektet
     var context = canvas.getContext("2d");
@@ -86,11 +73,30 @@ function drawImage(img, x, y) {
     context.stroke();
 }
 
+// Zoomer bildet ved aa legge til en andel av orginal bredden og storrelsen til orginal bredden / storrelsen. Andelen bestemmes av slideren, som brukere kontrollerer
+function zoomImage(factor) {
+    var img = document.getElementById("profilbildeimg");
+    img.width = factor * originalImgWidth + originalImgWidth;
+    img.height = factor * originalImgHeight + originalImgHeight;
+    drawImage(img, imgCoor.x, imgCoor.y);
+}
+
+// hjelpefunksjon for aa finne musposisjonen i canvasen til en event
+function getMousePosInCanvas(e) {
+    var rect = document.getElementById("redigeringscanvas").getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+}
+
+// Forbereder et bilde til visning fra en url strom
 function prepareAndDrawImage(file) {
     var img = document.getElementById("profilbildeimg");
     var canvas = document.getElementById("redigeringscanvas"); // fordi vi trenger DOM objekteter og kan ikke bruke jQuery objektet
     img.src = file.target.result;
 
+    // vi maa finne ut hvordan vi kan faa bildet til aa passe canvasen vaar uten at vi forandrer paa dimensjonene
     var changeRatio = 1;
     if (img.width > canvas.width) {
         changeRatio = canvas.width / img.width;
@@ -103,24 +109,29 @@ function prepareAndDrawImage(file) {
         img.height = canvas.height;
         img.width = changeRatio * img.width;
     }
+
     originalImgHeight = img.height;
     originalImgWidth = img.width;
     drawImage(img);
 }
 
-function zoomImage(factor) {
-    var img = document.getElementById("profilbildeimg");
-    img.width = factor * originalImgWidth + originalImgWidth;
-    img.height = factor * originalImgHeight + originalImgHeight;
-    drawImage(img, imgCoor.x, imgCoor.y);
-}
+function openFile(event){
 
-function getMousePosInCanvas(e) {
-    var rect = document.getElementById("redigeringscanvas").getBoundingClientRect();
-    return {
-        x: event.clientX - rect.left,
-        y: event.clientY - rect.top
-    };
+    var bildeInput = document.getElementById("bildeinput");
+    var imgElement = document.getElementById("profilbildeimg");
+
+    var file = bildeInput.files[0];
+
+    //er det egentlig et bilde?
+    if (!file.type.match('image.*')) {
+        alert("Det må være et jpg bilde!");  //lyver litt
+        return;
+    }
+
+    //laster bildefilen over i et img objekt
+    reader = new FileReader();
+    reader.onload = prepareAndDrawImage;
+    reader.readAsDataURL(file);
 }
 
 $(document).ready(function(){
@@ -156,13 +167,16 @@ $(document).ready(function(){
 
     canvas.mousemove(function(e) {
         if (dragging) {
-            movingImgCoor.x = imgCoor.x + getMousePosInCanvas(e).x - mouseDownCoor.x;
-            movingImgCoor.y = imgCoor.y + getMousePosInCanvas(e).y - mouseDownCoor.y;
+
+            // for aa faa til en dra illusjon maa vi ta de gamle bildekoordinatene og legge til forandringen som er gitt av eventen her minus der hvor brukeren trykket ned
+            movingImgCoor.x = imgCoor.x + (getMousePosInCanvas(e).x - mouseDownCoor.x);
+            movingImgCoor.y = imgCoor.y + (getMousePosInCanvas(e).y - mouseDownCoor.y);
             drawImage(document.getElementById("profilbildeimg"), movingImgCoor.x, movingImgCoor.y);
         }
     });
 
     canvas.mouseup(function(e) {
+        // naa oppdaterer vi bildekoordinatene vaare!
         imgCoor.x = movingImgCoor.x;
         imgCoor.y = movingImgCoor.y;
         dragging = false;
