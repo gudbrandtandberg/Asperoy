f2cfe59c4fcb19d2e01406d97f7dfb7f343982ec
Gudbrand <gudbrandduff@gmail.com>
2015-04-13 13:59:03 +0200

    fikset litt i ressurser

--- public/ressurser/index.php
+++ public/ressurser/index.php
@@ -3,13 +3,45 @@ include("../renderHelpers.php");
 renderHeaderWithTitle("ASPERØY - RESSURSER");
 ?>
 
-<p>
-    Her finner du mange nyttige Asperøy relaterte ressurser
-</p>
-<ul>
-    <li><a href="/resources/images/telt.pdf">Partytelt bruksanvisning</a></li>
-    <li><a href="/resources/images/morgenH12.pdf">Morgenbadet H12</a></li>
-</ul>
+<style>
+    ul > li > a, ul > li > a:hover {
+        color: inherit;
+    }
+    
+    .side h2, .side h3 { /*dette er duplikat, men tydeligvis nødvendig..*/
+	margin-top: 5px;
+    }
+</style>
+
+
+<div class="col-sm-12 col-md-6 side">
+    
+    <h2>Ressurser</h2>
+    <p>
+        Her finner du mange nyttige Asperøy-relaterte ressurser. Om du har noen dokumenter du ønsker å ha på denne
+        siden så er det bare å sende det inn til oss så skal det lastes opp.
+    </p>
+    
+    <h3>Morgenbadet</h3>
+    <ul>
+        <li><a href="/resources/images/morgenH12.pdf">Morgenbadet V12</a></li>
+        <li><a href="/resources/images/morgenH12.pdf">Morgenbadet H12</a></li>
+        <li><a href="/resources/images/morgenH12.pdf">Morgenbadet V13</a></li>
+        <li><a href="/resources/images/morgenH12.pdf">Morgenbadet H13</a></li>
+    </ul>
+    
+    <h3>GF-referater</h3>
+    
+</div>
+
+<div class="col-sm-12 col-md-6 side">
+    <h3>Bruksannvisninger</h3>
+        <ul>
+            <li><a href="/resources/images/telt.pdf">Partytelt bruksanvisning</a></li>
+        </ul>
+    <h3>Diverse</h3>
+</div>
+
 
 <?php
 renderFooter();
