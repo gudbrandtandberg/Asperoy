b262b6b106a2821b85d388cc88948075f88e9fcb
Eivind <eivind.m.bakke@gmail.com>
2015-04-15 22:06:20 +0800

    permissionsfix update

--- permissionsFix.sh
+++ permissionsFix.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
-chmod o+w model/*.xml
-chmod o+w model/*.json
-chmod go+w *.log
+sudo chown -R $USER:ladmin public/
+sudo chown -R $USER:ladmin model/
+sudo chown $USER:ladmin *.log
+chmod g+w model/*.xml
+chmod g+w model/*.json
+chmod g+w *.log
+chmod -R g+w public/
