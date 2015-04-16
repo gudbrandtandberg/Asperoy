2aa3e8116078ca01beb82a66d45c7ba4f7ea7448
Gudbrand <gudbrandduff@gmail.com>
2015-04-16 14:37:44 +0200

    ikkeno særlig

--- permissionsFix.sh
+++ permissionsFix.sh
@@ -2,7 +2,8 @@
 sudo chown -R $USER:ladmin public/
 sudo chown -R $USER:ladmin model/
 sudo chown $USER:ladmin *.log
-chmod g+w model/*.xml
-chmod g+w model/*.json
+chmod og+w model/*.xml
+chmod og+w model/*.json
 chmod g+w *.log
 chmod -R g+w public/
+
